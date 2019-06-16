5570ccecc1f7b304ed7d67a88b12747526f75e88
Akiicat <aaaa1379@gmail.com>
2019-06-16 12:50:25 +0800

    add 1 two sum go

---
+++ 1-two-sum/main.go
@@ -0,0 +1,15 @@
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+    fmt.Sprintf("%d %d", nums[0], target)
+
+    for i, num1 := range nums {
+        for j, num2 := range nums {
+            if (num1 + num2 == target && i != j) {
+                return []int{i, j}
+            }
+        }
+    }
+
+    return []int{0, 1}
+}
