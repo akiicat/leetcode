56e1002a040f4f22f677dbc664922dcf6212c444
Akiicat <aaaa1379@gmail.com>
2019-06-17 20:06:09 +0800

    add two-sum-II-input-array-is-sorted golang

---
+++ 167-two-sum-II-input-array-is-sorted/main.go
@@ -0,0 +1,31 @@
+package main
+import "fmt"
+
+func main() {
+  fmt.Printf("%v", twoSum([]int{2, 7, 11, 19}, 9))
+  fmt.Printf("%v", twoSum([]int{2, 3, 4}, 6))
+}
+
+// T: O(N)
+// M: O(N)
+// -- start --
+
+func twoSum(numbers []int, target int) []int {
+  start, end := 0, len(numbers) - 1
+
+  for start < end {
+    bias := (numbers[start] + numbers[end]) - target
+    if bias > 0 {
+      end--
+    } else if bias < 0 {
+      start++
+    } else {
+      break
+    }
+  }
+
+  return []int{start + 1, end + 1}
+}
+
+// -- end --
+
