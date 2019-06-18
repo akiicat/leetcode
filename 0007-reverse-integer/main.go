216ff40c145df05407214a35e5b975b60981c0fb
Akiicat <aaaa1379@gmail.com>
2019-06-19 02:57:15 +0800

    add 7-reverse-integer/main.go golang

---
+++ 0007-reverse-integer/main.go
@@ -0,0 +1,35 @@
+package main
+import "fmt"
+
+func main() {
+  fmt.Printf("Input: 123\nOutput: %d\nExpected: 321\n", reverse(123))
+  fmt.Printf("Input: -123\nOutput: %d\nExpected: -321\n", reverse(-123))
+  fmt.Printf("Input: 120\nOutput: %d\nExpected: 21\n", reverse(120))
+  fmt.Printf("Input: 1534236469\nOutput: %d\nExpected: 0\n", reverse(1534236469))
+}
+
+// T: O(N)
+// M: O(1)
+// -- start --
+
+func reverse(x int) int {
+  input := x // speed up, do not use x as computation
+  if input != int(int32(input)) {
+    return 0
+  }
+
+  reverse := 0
+  for (input != 0) {
+    reverse = 10 * reverse + input % 10
+    input = input / 10
+  }
+
+  if reverse != int(int32(reverse)) {
+    return 0
+  }
+
+  return reverse
+}
+
+// -- end --
+
