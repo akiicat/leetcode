9b12fe727a9a6c888e5b0c82c61a5652f66f79a5
Akiicat <aaaa1379@gmail.com>
2019-06-28 01:15:52 +0800

    add 9-palindrome-number/main.go golang

---
+++ 0009-palindrome-number/main.go
@@ -0,0 +1,34 @@
+package main
+import "fmt"
+// import "math/bits"
+
+func main() {
+  fmt.Printf("Input:  0\nOutput: %t\nExpect: true\n", isPalindrome(0))
+  fmt.Printf("Input:  1\nOutput: %t\nExpect: true\n", isPalindrome(1))
+  fmt.Printf("Input:  121\nOutput: %t\nExpect: true\n", isPalindrome(121))
+  fmt.Printf("Input:  -121\nOutput: %t\nExpect: false\n", isPalindrome(-121))
+  fmt.Printf("Input:  313\nOutput: %t\nExpect: true\n", isPalindrome(313))
+  fmt.Printf("Input:  1122\nOutput: %t\nExpect: false\n", isPalindrome(1122))
+}
+
+// T: O(log(N))
+// M: O(1)
+// -- start --
+
+func isPalindrome(x int) bool {
+  if x < 0 {
+    return false
+  }
+
+  y := 0
+  tmp := x
+  for tmp != 0 {
+    y = 10 * y + tmp % 10
+    tmp /= 10
+  }
+
+  return x == y
+}
+
+// -- end --
+
