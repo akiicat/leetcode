683eb3f4ac7dc71c459f9681d84b96f5a0b84d34
Akiicat <aaaa1379@gmail.com>
2019-07-11 04:53:51 +0800

    Update 0066-plus-one/main.go

--- 0066-plus-one/main.go
+++ 0066-plus-one/main.go
@@ -2,47 +2,38 @@ package main
 import "fmt"
 
 func main() {
-  i, o := []string{"11", "1"}, "100"
+  i, o := []int{1,2,3}, []int{1,2,4}
   fmt.Printf("Input:  %v\n", i)
-  fmt.Printf("Output: %v\nExpect: %v\n", addBinary(i[0], i[1]), o)
+  fmt.Printf("Output: %v\nExpect: %v\n", plusOne(i), o)
 
-  i, o = []string{"1010", "1011"}, "10101"
+  i, o = []int{4,3,2,1}, []int{4,3,2,2}
   fmt.Printf("Input:  %v\n", i)
-  fmt.Printf("Output: %v\nExpect: %v\n", addBinary(i[0], i[1]), o)
+  fmt.Printf("Output: %v\nExpect: %v\n", plusOne(i), o)
 }
 
 // T: O(N)
 // M: O(1)
 // -- start --
 
-func addBinary(a string, b string) string {
-  b2s := map[bool]string{false: "0", true: "1"}
+func plusOne(digits []int) []int {
+  carry := 1
+  for i := len(digits) - 1; i >= 0; i-- {
+    digits[i] += carry
 
-  rtn := ""
-  s, c := false, false // sum, carry
-  for i, j := len(a)-1, len(b)-1; i >= 0 || j >= 0; i, j = i-1, j-1 {
-    s, c = adder(i >= 0 && a[i] == '1', j >= 0 && b[j] == '1', c)
-    rtn = b2s[s] + rtn
+    // digits[i], carry = sum % 10, sum / 10
+    if digits[i] >= 10 {
+      digits[i] -= 10
+    } else {
+      carry = 0
+    }
+
   }
 
-  if c {
-    return "1" + rtn
+  if carry == 1 {
+    return append([]int{carry}, digits...)
   }
 
-  return rtn
-}
-
-func adder(a, b, c bool) (bool, bool) {
-  // sum: a ^ b ^ c
-  // carry: (a & b) | (b & c) | (c & a)
-  return ((a != b) != c), a && b || b && c || c && a
-}
-
-func max(a, b int) int {
-  if a > b {
-    return a
-  }
-  return b
+  return digits
 }
 
 // -- end --
