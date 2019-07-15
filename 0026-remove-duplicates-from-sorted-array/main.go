a36a6c80ba38829c17af279284780c00fbf6a87e
Akiicat <aaaa1379@gmail.com>
2019-07-16 04:15:33 +0800

    add 26-remove-duplicates-from-sorted-array/main.go golang

---
+++ 0026-remove-duplicates-from-sorted-array/main.go
@@ -0,0 +1,34 @@
+package main
+import "fmt"
+
+func main() {
+  i, o := []int{1,1,2}, 2
+  fmt.Println("Input:  ", i)
+  fmt.Println("Output: ", i[0:o], removeDuplicates(i))
+  fmt.Println("Expect: ", []int{1,2}, o)
+
+  i, o = []int{0,0,1,1,1,2,2,3,3,4}, 5
+  fmt.Println("Input:  ", i)
+  fmt.Println("Output: ", i[0:o], removeDuplicates(i))
+  fmt.Println("Expect: ", []int{0,1,2,3,4}, o)
+}
+
+// T: O(N)
+// M: O(1)
+// -- start --
+
+func removeDuplicates(nums []int) int {
+  cur := 0
+
+  for _, num := range nums {
+    if nums[cur] != num {
+      cur++
+      nums[cur] = num
+    }
+  }
+
+  return cur + 1
+}
+
+// -- end --
+
