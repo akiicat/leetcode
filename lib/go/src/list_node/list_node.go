4cf478c0c4475a027bbc078d2e28a73bcf459e57
Akiicat <aaaa1379@gmail.com>
2019-07-20 10:17:58 +0800

    add lib/go/src/list_node/list_node.go

---
+++ lib/go/src/list_node/list_node.go
@@ -0,0 +1,42 @@
+package list_node
+import "fmt"
+import "strings"
+
+type ListNode struct {
+  Val int
+  Next *ListNode
+}
+
+func NewListNode(nums []int) *ListNode {
+  head := make([]ListNode, len(nums))
+
+  for i, num := range nums {
+    head[i].Val = num
+    if i < len(nums) - 1 {
+      head[i].Next = &head[i + 1]
+    }
+  }
+
+  return &head[0]
+}
+
+func (l *ListNode) Printf() {
+  list := l
+  for list != nil {
+    fmt.Printf("%+v->", list.Val)
+    list = list.Next
+  }
+  fmt.Printf("NULL")
+}
+
+func (l *ListNode) Sprintf() string {
+  var s strings.Builder
+  list := l
+  for list != nil {
+    fmt.Fprintf(&s, "%+v->", list.Val)
+    list = list.Next
+  }
+  fmt.Fprintf(&s, "NULL")
+  return s.String()
+}
+
