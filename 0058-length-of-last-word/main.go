bdd9f960fb18debc661693cb685f58a176d132b6
Akiicat <aaaa1379@gmail.com>
2019-07-30 13:48:09 +0800

    add 58-length-of-last-word/main.go golang

---
+++ 0058-length-of-last-word/main.go
@@ -0,0 +1,75 @@
+package main
+import "fmt"
+
+func main() {
+  i, o := "Hello World", 5
+  fmt.Printf("Input:  %s\n", i)
+  fmt.Printf("Output: %d\n", lengthOfLastWord(i))
+  fmt.Printf("Expect: %d\n", o)
+
+  i, o = "Hello ", 5
+  fmt.Printf("Input:  %s\n", i)
+  fmt.Printf("Output: %d\n", lengthOfLastWord(i))
+  fmt.Printf("Expect: %d\n", o)
+
+  i, o = "  ", 0
+  fmt.Printf("Input:  %s\n", i)
+  fmt.Printf("Output: %d\n", lengthOfLastWord(i))
+  fmt.Printf("Expect: %d\n", o)
+
+  i, o = " ", 0
+  fmt.Printf("Input:  %s\n", i)
+  fmt.Printf("Output: %d\n", lengthOfLastWord(i))
+  fmt.Printf("Expect: %d\n", o)
+
+  i, o = "", 0
+  fmt.Printf("Input:  %s\n", i)
+  fmt.Printf("Output: %d\n", lengthOfLastWord(i))
+  fmt.Printf("Expect: %d\n", o)
+}
+
+// T: O(N)
+// M: O(1)
+// -- start --
+
+func lengthOfLastWord(s string) int {
+  start := len(s) - 1
+
+  for start >= 0 {
+    if s[start] != ' ' {
+      break
+    }
+    start--
+  }
+
+  for i := start - 1; i >= 0; i-- {
+    if s[i] == ' ' {
+      return start - i
+    }
+  }
+
+  return start + 1
+}
+
+func lengthOfLastWordBoolean(s string) int {
+	length := 0
+	foundCharacter := false
+
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == ' ' {
+			if foundCharacter {
+				break
+			}
+
+			continue
+		}
+
+		length++
+		foundCharacter = true
+	}
+
+	return length
+}
+
+// -- end --
+
