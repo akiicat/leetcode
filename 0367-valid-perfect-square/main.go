c643dc308bfff1b179147b186b2db12aef86bc00
Akiicat <aaaa1379@gmail.com>
2019-08-01 16:02:04 +0800

    add 367-valid-perfect-square/main.go golang

---
+++ 0367-valid-perfect-square/main.go
@@ -0,0 +1,37 @@
+package main
+import "fmt"
+import "math"
+
+func main() {
+  i, o := 16, true
+  fmt.Printf("Input:  %d\n", i)
+  fmt.Printf("Output: %t\n", isPerfectSquare(i))
+  fmt.Printf("Expect: %t\n", o)
+
+  i, o = 14, false
+  fmt.Printf("Input:  %d\n", i)
+  fmt.Printf("Output: %t\n", isPerfectSquare(i))
+  fmt.Printf("Expect: %t\n", o)
+}
+
+// leetcode 69.
+// T: O(log(N)) // the time complexity of Sqrt function if log(N)
+// M: O(1)
+// -- start --
+
+func isPerfectSquare(num int) bool {
+	i := 1
+	for num > 0 {
+		num -= i
+		i += 2
+	}
+	return num == 0
+}
+
+func isPerfectSquareMath(num int) bool {
+  sqrt := math.Sqrt(float64(num))
+  return sqrt == float64(int(sqrt))
+}
+
+// -- end --
+
