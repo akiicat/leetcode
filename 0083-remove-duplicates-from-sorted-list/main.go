ff104f0ae91db09619c682ab17b96719d88cb46b
Akiicat <aaaa1379@gmail.com>
2019-08-11 14:40:37 +0800

    add 83-remove-duplicates-from-sorted-list/main.go golang

---
+++ 0083-remove-duplicates-from-sorted-list/main.go
@@ -0,0 +1,54 @@
+package main
+import "fmt"
+import . "list_node"
+
+// type ListNode struct {
+//   Val int
+//   Next *ListNode
+// }
+
+func main() {
+  i, o := NewListNode([]int{1,1,2}), NewListNode([]int{1,2})
+  fmt.Printf("Input:  %s\n", i.Sprintf())
+  fmt.Printf("Output: %s\n", deleteDuplicates(i).Sprintf())
+  fmt.Printf("Expect: %s\n", o.Sprintf())
+
+  i, o = NewListNode([]int{1,1,2,3,3}), NewListNode([]int{1,2,3})
+  fmt.Printf("Input:  %s\n", i.Sprintf())
+  fmt.Printf("Output: %s\n", deleteDuplicates(i).Sprintf())
+  fmt.Printf("Expect: %s\n", o.Sprintf())
+
+  i, o = NewListNode([]int{1,1,1}), NewListNode([]int{1})
+  fmt.Printf("Input:  %s\n", i.Sprintf())
+  fmt.Printf("Output: %s\n", deleteDuplicates(i).Sprintf())
+  fmt.Printf("Expect: %s\n", o.Sprintf())
+
+  i, o = NewListNode([]int{}), NewListNode([]int{})
+  fmt.Printf("Input:  %s\n", i.Sprintf())
+  fmt.Printf("Output: %s\n", deleteDuplicates(i).Sprintf())
+  fmt.Printf("Expect: %s\n", o.Sprintf())
+}
+
+// leetcode 26.
+// T: O(n)
+// M: O(1)
+// -- start --
+
+func deleteDuplicates(head *ListNode) *ListNode {
+  if head == nil || head.Next == nil {
+    return head
+  }
+  cur := head
+
+  for cur.Next != nil {
+    if cur.Val ==  cur.Next.Val {
+      cur.Next = cur.Next.Next
+    } else {
+      cur = cur.Next
+    }
+  }
+
+  return head
+}
+
+// -- end --
