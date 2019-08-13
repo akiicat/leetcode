221900b3041081419482ef653f866a10e247dcfe
Akiicat <aaaa1379@gmail.com>
2019-08-13 16:42:48 +0800

    add 119-pascals-triangle-ii/main.go golang

---
+++ 0119-pascals-triangle-ii/main.go
@@ -0,0 +1,43 @@
+package main
+import "fmt"
+
+func main() {
+  i, o := 3, []int{1,3,3,1}
+  fmt.Printf("Input:  %d\n", i)
+  fmt.Printf("Output: %v\n", getRow(i))
+  fmt.Printf("Expect: %v\n", o)
+
+  i, o = 0, []int{1}
+  fmt.Printf("Input:  %d\n", i)
+  fmt.Printf("Output: %v\n", getRow(i))
+  fmt.Printf("Expect: %v\n", o)
+
+  i, o = 4, []int{1,4,6,4,1}
+  fmt.Printf("Input:  %d\n", i)
+  fmt.Printf("Output: %v\n", getRow(i))
+  fmt.Printf("Expect: %v\n", o)
+}
+
+// https://leetcode.com/articles/pascals-triangle/
+// T: O(n ** 2) n for numRows
+// M: O(n)
+// -- start --
+
+// Dynamic Programming
+// T: O(n ** 2) n for numRows
+// M: O(n)
+func getRow(rowIndex int) []int {
+  tri := make([]int, rowIndex + 1)
+  tri[0] = 1
+
+  for r := 1; r < rowIndex + 1; r++ {
+    for c := r; c > 0; c-- {
+      tri[c] = tri[c] + tri[c-1]
+    }
+  }
+
+  return tri
+}
+
+// -- end --
+
