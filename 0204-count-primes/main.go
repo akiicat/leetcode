2ee387229c7680c5fa2642cba7a2d8365707f2d5
Akiicat <aaaa1379@gmail.com>
2019-08-17 20:54:11 +0800

    add 0204-count-primes/main.go golang

---
+++ 0204-count-primes/main.go
@@ -0,0 +1,72 @@
+package main
+import "fmt"
+
+func main() {
+  i, o := 10, 4
+  fmt.Printf("Input:  %d\n", i)
+  fmt.Printf("Output: %d\n", countPrimes(i))
+  fmt.Printf("Expect: %d\n", o)
+
+  i, o = 20, 8
+  fmt.Printf("Input:  %d\n", i)
+  fmt.Printf("Output: %d\n", countPrimes(i))
+  fmt.Printf("Expect: %d\n", o)
+}
+
+// T: O(n)
+// M: O(n)
+// -- start --
+
+// T: O(n)
+// M: O(n)
+func countPrimes(n int) int {
+	if n < 3 {
+		return 0
+	}
+
+	isComposite := make([]bool, n)
+	count := n / 2
+	for i := 3; i*i < n; i += 2 {
+
+		if isComposite[i] {
+			continue
+		}
+		for j := i * i; j < n; j += 2 * i {
+			if !isComposite[j] {
+				count--
+				isComposite[j] = true
+			}
+		}
+	}
+	return count
+}
+
+// T: O(n ** 2)
+// M: O(1)
+func countPrimesIsPrime(n int) int {
+  count := 0
+
+  for i := 1; i < n; i++ {
+    if isPrime(i) {
+      count++
+    }
+  }
+
+  return count;
+}
+
+func isPrime(num int) bool {
+  if num <= 1 {
+    return false
+  }
+
+  for i := 2; i * i <= num; i++ {
+    if num % i == 0 {
+      return false
+    }
+  }
+
+  return true
+}
+
+// -- end --
