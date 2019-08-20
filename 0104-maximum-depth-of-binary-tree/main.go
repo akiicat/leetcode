f8fdb28eb679959d262e8798679203c4a1b29e39
Akiicat <aaaa1379@gmail.com>
2019-08-21 01:36:35 +0800

    add 0104-maximum-depth-of-binary-tree/main.go golang

---
+++ 0104-maximum-depth-of-binary-tree/main.go
@@ -0,0 +1,39 @@
+package main
+import "fmt"
+import . "tree_node"
+
+// type TreeNode struct {
+//     Val int
+//     Left *TreeNode
+//     Right *TreeNode
+// }
+
+func main() {
+  i, o := NewTreeNode("3,9,20,null,null,15,7"), 3
+  fmt.Printf("Input:  %s\n", i.Sprintf())
+  fmt.Printf("Output: %d\n", maxDepth(i))
+  fmt.Printf("Expect: %d\n", o)
+}
+
+// T: O(n)
+// M: O(h)
+// -- start --
+
+func maxDepth(root *TreeNode) int {
+  if root == nil {
+    return 0
+  }
+
+  return Max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func Max(a, b int) int {
+  if a > b {
+    return a
+  }
+  return b
+}
+
+
+// -- end --
+
