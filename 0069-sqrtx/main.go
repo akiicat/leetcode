fb360ffae599b73f537cdf4da43e3f12f576b11a
Akiicat <aaaa1379@gmail.com>
2019-08-21 01:39:50 +0800

    add binary search method to 0069-sqrtx/main.go

--- 0069-sqrtx/main.go
+++ 0069-sqrtx/main.go
@@ -3,34 +3,39 @@ import "fmt"
 import "math"
 
 func main() {
-  i, o := 1, 1
+  i, o := 0, 0
   fmt.Printf("Input:  %d\n", i)
-  fmt.Printf("Output: %d\n", mySqrt(i))
+  fmt.Printf("Output: %d\n", mySqrtBinarySearch(i))
+  fmt.Printf("Expect: %d\n", o)
+
+  i, o = 1, 1
+  fmt.Printf("Input:  %d\n", i)
+  fmt.Printf("Output: %d\n", mySqrtBinarySearch(i))
   fmt.Printf("Expect: %d\n", o)
 
   i, o = 4, 2
   fmt.Printf("Input:  %d\n", i)
-  fmt.Printf("Output: %d\n", mySqrt(i))
+  fmt.Printf("Output: %d\n", mySqrtBinarySearch(i))
   fmt.Printf("Expect: %d\n", o)
 
   i, o = 8, 2
   fmt.Printf("Input:  %d\n", i)
-  fmt.Printf("Output: %d\n", mySqrt(i))
+  fmt.Printf("Output: %d\n", mySqrtBinarySearch(i))
   fmt.Printf("Expect: %d\n", o)
 
   i, o = 9, 3
   fmt.Printf("Input:  %d\n", i)
-  fmt.Printf("Output: %d\n", mySqrt(i))
+  fmt.Printf("Output: %d\n", mySqrtBinarySearch(i))
   fmt.Printf("Expect: %d\n", o)
 
   i, o = 10, 3
   fmt.Printf("Input:  %d\n", i)
-  fmt.Printf("Output: %d\n", mySqrt(i))
+  fmt.Printf("Output: %d\n", mySqrtBinarySearch(i))
   fmt.Printf("Expect: %d\n", o)
 
   i, o = 36, 6
   fmt.Printf("Input:  %d\n", i)
-  fmt.Printf("Output: %d\n", mySqrt(i))
+  fmt.Printf("Output: %d\n", mySqrtBinarySearch(i))
   fmt.Printf("Expect: %d\n", o)
 }
 
@@ -60,6 +65,27 @@ func mySqrt(x int) int {
       l = mid
     }
   }
+}
+
+func mySqrtBinarySearch(x int) int {
+  return binarySearch(1, x, x)
+}
+
+func binarySearch(start, end, num int) int {
+  if start > end {
+    return 0
+  }
+
+  mid := (start + end) / 2
+  if mid * mid <= num && (mid + 1) * (mid + 1) > num {
+    return mid
+  }
+
+  if mid * mid > num {
+    return binarySearch(start, mid - 1, num)
+  }
+
+  return binarySearch(mid + 1, end, num)
 }
 
 func mySqrtMath(x int) int {
