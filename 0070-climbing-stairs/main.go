e0131c8092d6a9b8891010377c6afd46e4251c96
Akiicat <aaaa1379@gmail.com>
2019-08-21 01:41:04 +0800

    add formula solution to 0070-climbing-stairs/main.go

--- 0070-climbing-stairs/main.go
+++ 0070-climbing-stairs/main.go
@@ -1,5 +1,6 @@
 package main
 import "fmt"
+import "math"
 
 func main() {
   i, o := 2, 2
@@ -21,10 +22,16 @@ func main() {
   fmt.Printf("Input:  %d\n", i)
   fmt.Printf("Output: %d\n", climbStairs(i))
   fmt.Printf("Expect: %d\n", o)
+
+  i, o = 9, 55
+  fmt.Printf("Input:  %d\n", i)
+  fmt.Printf("Output: %d\n", climbStairs(i))
+  fmt.Printf("Expect: %d\n", o)
 }
 
-// fibonacci
-// T: O(N)
+// leetcode 509. fibonacci
+// https://leetcode.com/articles/climbing-stairs/
+// T: O(log(N))
 // M: O(1)
 // -- start --
 
@@ -40,14 +47,23 @@ func climbStairs(n int) int {
   return x2
 }
 
-// T: O(N ** 2)
+// T: O(log(N))
+// M: O(1)
+func climbStairsFormula(n int) int {
+  sqrt5 := math.Sqrt(5)
+  fibn := math.Pow((1+sqrt5)/2, float64(n+1)) - math.Pow((1-sqrt5)/2, float64(n+1))
+  return int(math.Round(fibn / sqrt5));
+}
+
+
+// T: O(2 ** N)
 // M: O(1)
 func climbStairsRecursive(n int) int {
   if n == 0 || n == 1 {
     return 1
   }
 
-  return climbStairs(n-1) + climbStairs(n-2)
+  return climbStairsRecursive(n-1) + climbStairsRecursive(n-2)
 }
 
 
