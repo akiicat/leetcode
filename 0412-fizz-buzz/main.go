aeba8642f41b8068283724ecc5f0fff90d8d0ab7
Akiicat <aaaa1379@gmail.com>
2019-10-01 18:18:16 +0800

    add 0412-fizz-buzz/main.go golang

---
+++ 0412-fizz-buzz/main.go
@@ -0,0 +1,73 @@
+package main
+import "fmt"
+import "strconv"
+
+func main() {
+  i, o := 15, []string{
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"}
+  fmt.Printf("Input:  %d\n", i)
+  fmt.Printf("Output: %v\n", fizzBuzz(i))
+  fmt.Printf("Expect: %v\n", o)
+}
+
+// T: O(n)
+// M: O(1)
+// -- start --
+
+func fizzBuzz(n int) []string {
+  rtn := []string{}
+
+  for i := 1; i <= n; i++ {
+    s := strconv.Itoa(i)
+    if i % 15 == 0 {
+      s = "FizzBuzz"
+    } else if i % 5 == 0 {
+      s = "Buzz"
+    } else if i % 3 == 0 {
+      s = "Fizz"
+    }
+    rtn = append(rtn, s)
+  }
+
+  return rtn
+}
+
+
+func fizzBuzzMethodOne(n int) []string {
+  rtn := make([]string, n)
+
+  for i := 0; i < n; i++ {
+    m3, m5 := (i+1) % 3 == 0, (i+1) % 5 == 0
+
+    if m3 {
+      rtn[i] = fmt.Sprintf("%s%s", rtn[i], "Fizz")
+    }
+
+    if m5 {
+      rtn[i] = fmt.Sprintf("%s%s", rtn[i], "Buzz")
+    }
+
+    if !m3 && !m5 {
+      rtn[i] = fmt.Sprintf("%d", i+1)
+    }
+  }
+
+  return rtn
+}
+
+// -- end --
+
