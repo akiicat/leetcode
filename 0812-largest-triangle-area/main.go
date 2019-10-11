77b883828c647c317027a3defe6b46f2d8be8748
Akiicat <aaaa1379@gmail.com>
2019-10-11 13:40:07 +0800

    add 0812-largest-triangle-area/main.go golang

---
+++ 0812-largest-triangle-area/main.go
@@ -0,0 +1,54 @@
+package main
+import "fmt"
+
+func main() {
+  i, o := [][]int{[]int{0,0},[]int{0,1},[]int{1,0},[]int{0,2},[]int{2,0}}, 2.0
+  fmt.Printf("Input:  %v\n", i)
+  fmt.Printf("Output: %f\n", largestTriangleArea(i))
+  fmt.Printf("Expect: %f\n", o)
+}
+
+// T: O(n*3)
+// M: O(1)
+// -- start --
+
+func largestTriangleArea(points [][]int) float64 {
+  if len(points) < 3 {
+    return 0
+  }
+
+  max := 0.0
+
+  for i := 0; i < len(points)-2; i++ {
+    for j := 0; j < len(points)-1; j++ {
+      for k := 0; k < len(points); k++ {
+        area := Area(points[i], points[j], points[k])
+
+        if area > max {
+          max = area
+        }
+      }
+    }
+  }
+
+  return max / 2
+}
+
+func Area(p1, p2, p3 []int) float64 {
+  a0 := p2[0] - p1[0]
+  a1 := p2[1] - p1[1]
+  b0 := p3[0] - p1[0]
+  b1 := p3[1] - p1[1]
+
+  return float64(Abs(a0 * b1 - a1 * b0))
+}
+
+func Abs(a int) int {
+  if a < 0 {
+    return -a
+  }
+  return a
+}
+
+// -- end --
+
