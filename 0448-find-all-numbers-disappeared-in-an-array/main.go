17a9ce3a52ff69e6cfa26712cb4da2b9a72505ef
Akiicat <aaaa1379@gmail.com>
2019-10-14 17:14:44 +0800

    add 0448-find-all-numbers-disappeared-in-an-array/main.go golang

---
+++ 0448-find-all-numbers-disappeared-in-an-array/main.go
@@ -0,0 +1,64 @@
+package main
+import "fmt"
+
+func main() {
+  i, o := []int{4,3,2,7,8,2,3,1}, []int{5,6}
+  fmt.Printf("Input:  %v\n", i)
+  fmt.Printf("Output: %d\n", findDisappearedNumbers(i))
+  fmt.Printf("Expect: %d\n", o)
+
+  i, o = []int{4,7,2,7,8,8,3,1}, []int{5,6}
+  fmt.Printf("Input:  %v\n", i)
+  fmt.Printf("Output: %d\n", findDisappearedNumbers(i))
+  fmt.Printf("Expect: %d\n", o)
+}
+
+// T: O(n)
+// M: O(1)
+// -- start --
+
+// T: O(n)
+// M: O(1)
+func findDisappearedNumbers(nums []int) []int {
+  rtn := make([]int, 0)
+
+  for _, num := range nums {
+    if num < 0 {
+      num *= -1
+    }
+    if nums[num-1] > 0 {
+      nums[num-1] *= -1
+    }
+  }
+
+  for i, num := range nums {
+    if num > 0 {
+      rtn = append(rtn, i+1)
+    }
+  }
+
+  return rtn
+}
+
+// T: O(n)
+// M: O(n)
+func findDisappearedNumbersMap(nums []int) []int {
+  n := len(nums)
+  m := make([]bool, n + 1)
+  rtn := []int{}
+
+  for _, num := range nums {
+    m[num] = true
+  }
+
+  for i := 1; i < n+1; i++ {
+    if m[i] == false {
+      rtn = append(rtn, i)
+    }
+  }
+
+  return rtn
+}
+
+// -- end --
+
