5cce6d1ecc280829ee1303b460220a535e92bf7f
Akiicat <aaaa1379@gmail.com>
2019-12-20 23:57:46 +0800

    add 0696-count-binary-substrings/main.go golang

---
+++ src/0696-count-binary-substrings/main.go
@@ -0,0 +1,75 @@
+package main
+import "fmt"
+
+func main() {
+  i, o := "00110011", 6
+  fmt.Printf("Input:  %s\n", i)
+  fmt.Printf("Output: %d\n", countBinarySubstrings(i))
+  fmt.Printf("Expect: %d\n", o)
+
+  i, o = "10101", 4
+  fmt.Printf("Input:  %s\n", i)
+  fmt.Printf("Output: %d\n", countBinarySubstrings(i))
+  fmt.Printf("Expect: %d\n", o)
+}
+
+// T: O(n)
+// M: O(1)
+// -- start --
+
+// Linear Scan
+// T: O(n)
+// M: O(1)
+func countBinarySubstrings(s string) int {
+  pre, cur := s[0], 1
+  sum, res := 0, 0
+  for i := 1; i < len(s); i++ {
+    if pre == s[i] {
+      cur++
+      continue
+    }
+
+    sum += Min(cur, res)
+    pre, cur, res = s[i], 1, cur
+  }
+  sum += Min(cur, res)
+
+  return sum
+}
+
+
+// Group By Character
+// T: O(n)
+// M: O(n)
+func countBinarySubstringsArray(s string) int {
+  m := []int{}
+
+  p, c := s[0], 1
+  for i := 1; i < len(s); i++ {
+    if p != s[i] {
+      m = append(m, c)
+      p, c = s[i], 0
+    }
+
+    c++
+  }
+  m = append(m, c)
+
+
+  sum := 0
+  for i := 1; i < len(m); i++ {
+    sum += Min(m[i-1], m[i])
+  }
+
+  return sum
+}
+
+func Min(a, b int) int {
+  if a > b {
+    return b
+  }
+  return a
+}
+
+// -- end --
+
