ed2dfe02aa0d2712661c5a8b60400c70fc4497fd
Akiicat <aaaa1379@gmail.com>
2019-12-23 22:54:55 +0800

    add 0717-1-bit-and-2-bit-characters/main.go golang

---
+++ src/0717-1-bit-and-2-bit-characters/main.go
@@ -0,0 +1,84 @@
+package main
+import "fmt"
+
+func main() {
+  i, o := []int{1,0,0}, true
+  fmt.Printf("Input:  %v\n", i)
+  fmt.Printf("Output: %t\n", isOneBitCharacter(i))
+  fmt.Printf("Expect: %t\n", o)
+
+  i, o = []int{1,1,1,0}, false
+  fmt.Printf("Input:  %v\n", i)
+  fmt.Printf("Output: %t\n", isOneBitCharacter(i))
+  fmt.Printf("Expect: %t\n", o)
+
+  i, o = []int{0}, true
+  fmt.Printf("Input:  %v\n", i)
+  fmt.Printf("Output: %t\n", isOneBitCharacter(i))
+  fmt.Printf("Expect: %t\n", o)
+}
+
+// https://leetcode.com/articles/1-bit-and-2-bit-characters/
+// T: O(n)
+// M: O(1)
+// -- start --
+
+
+// T: O(n)
+// M: O(1)
+func isOneBitCharacter(bits []int) bool {
+
+  i := 0
+  for i < len(bits) - 1 {
+    i += bits[i] + 1
+  }
+  fmt.Println(i)
+
+  return i == len(bits) - 1
+}
+
+// T: O(n)
+// M: O(1)
+func isOneBitCharacterGreedy(bits []int) bool {
+  i := len(bits) - 2
+  for i >= 0 && bits[i] > 0 {
+    i--
+  }
+  return (len(bits) - i) % 2 == 0
+}
+
+// T: O(n)
+// M: O(1)
+func isOneBitCharacterOneBool(bits []int) bool {
+  rtn := false
+
+  for i := 0; i < len(bits); i++ {
+    if bits[i] == 0 {
+      rtn = true
+    } else {
+      rtn = false
+      i++
+    }
+  }
+
+  return rtn
+}
+
+// T: O(n)
+// M: O(1)
+func isOneBitCharacterTwoBool(bits []int) bool {
+  pre, rtn := bits[0] == 0, bits[0] == 0
+  for i := 1; i < len(bits); i++ {
+    if rtn == false {
+      pre, rtn = rtn, true
+      continue
+    }
+
+    pre, rtn = rtn, bits[i] == 0
+  }
+
+  return pre
+}
+
+// -- end --
+
