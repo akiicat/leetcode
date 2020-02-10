1e05e60df8200aa8e7fbfac2b7175821f58dc0eb
Akiicat <aaaa1379@gmail.com>
2020-02-10 23:18:19 +0800

    update testing helper

--- pkg/testing_helper/testing_helper.go
+++ pkg/testing_helper/testing_helper.go
@@ -1,30 +1,57 @@
 package testing_helper
 
 // import . "main/pkg/tree_node"
-// import . "main/pkg/list_node"
+import . "main/pkg/list_node"
 import "fmt"
 import "testing"
 
-func S(i ...interface{}) string {
+type Value struct {
+  Val string
+  Err error
+}
+
+func S(i ...interface{}) *Value {
   if len(i) > 1 {
-    return fmt.Sprintf("%s %s", S(i[0]), S(i[1:]...))
+    a, b := S(i[0]), S(i[1:]...)
+
+    if a.Err != nil {
+      return a
+    }
+    if b.Err != nil {
+      return b
+    }
+
+    return &Value{Val: a.Val + " " + b.Val}
   }
 
   switch v := i[0].(type) {
   case []string:
-    return fmt.Sprintf("%v", v)
+    return &Value{Val: fmt.Sprintf("%v", v)}
   case string:
-    return fmt.Sprintf("%s", v)
+    return &Value{Val: fmt.Sprintf("%s", v)}
   case int:
-    return fmt.Sprintf("%d", v)
+    return &Value{Val: fmt.Sprintf("%d", v)}
+  case *ListNode:
+    return &Value{Val: fmt.Sprintf("%s", v.ToStr())}
   default:
-    return fmt.Sprintf("failed at type %T", v)
+    return &Value{Err: fmt.Errorf("failed at type %T", v)}
   }
 }
 
-func T(t *testing.T, i, r, o string) {
-  if r != o {
-    t.Errorf("\nInput:  %s\nOutput: %s\nExpect: %s\n", i, r, o)
+func T(t *testing.T, i, r, o *Value) {
+
+  if i.Err != nil {
+    t.Fatalf(i.Err.Error())
+  }
+  if r.Err != nil {
+    t.Fatalf(r.Err.Error())
+  }
+  if o.Err != nil {
+    t.Fatalf(o.Err.Error())
+  }
+
+  if r.Val != o.Val {
+    t.Errorf("\nInput:  %s\nOutput: %s\nExpect: %s\n", i.Val, r.Val, o.Val)
   }
 }
 
